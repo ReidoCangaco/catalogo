117ffd32775cc9527946a451f98e7fb63c5132dc
Pedro <seuemail@exemplo.com>
2026-07-16 14:59:16 -0300

    Nikbar add

--- script.js
+++ script.js
@@ -171,6 +171,37 @@
       image: "https://media.discordapp.net/attachments/1276211560993259572/1527368073550626816/BCO.701d7b97-99ef-44b1-ab52-884267b5f17c.png?ex=6a5a67dd&is=6a59165d&hm=51fa8d8a8de0ba663775451f9cf2c63cedf389793ded59cccbbc303302f0c85b&=&format=webp&quality=lossless&width=960&height=960",
       description: "Ótima opção para quem busca design, sabor intenso e preço em promoção.",
     },
+    /* ==========================================================================
+       NIKBAR 10.000
+       ========================================================================== */
+      {
+      id: "nikbar-10000",
+      name: "NIKBAR 10.000",
+      puffs: "10.000 tragadas",
+      flavors: [
+        "Strawberry ice",
+        "Menthol",
+        "Watermelon ice",
+        "Strawberry Kiwi",
+        "Grape Strawberry",
+        "Passion Sour Kiwi",
+        "Grape Ice",
+        "Watermelon Bubblegum",
+        "Pineapple Ice",
+        "Green apple ice",
+        "Cherry watermelon",
+        "Strawberry banana",
+      ],
+      originalPrice: 115.0,
+      promoPrice: 99.9,
+      badge: "Mais Opções",
+      accent: "Laranja",
+      featured: false,
+      image: "https://media.discordapp.net/attachments/1276211560993259572/1527373091779514408/BCO.b99a9943-8800-41a8-9c48-b6b4b102ec11.png?ex=6a5a6c8a&is=6a591b0a&hm=3524a1e98c4ed8ab618f0052e674ce0324a3abd7589400c55ad0a0fadb4f85e9&=&format=webp&quality=lossless&width=960&height=960",
+      description: "Ótima opção para quem busca momento, sabor intenso e preço em promoção.",
+    },
+
+
   ];
 
   PRODUCTS.forEach((product) => {
