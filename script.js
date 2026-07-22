7d01739c8c59bf3b5d40fb70357f755802503cf6
Pedro <seuemail@exemplo.com>
2026-07-22 13:57:44 -0300

    atualização

--- script.js
+++ script.js
@@ -434,7 +434,7 @@
         </div>
         <div class="card-footer">
           <a class="btn btn-primary btn-product-options" href="${productPageLink}">
-            👁 Ver sabores >
+            Escolher sabor
           </a>
         </div>
       </article>
